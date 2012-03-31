b634f86a2aba81d4eaadebdf30f7d1500dc956e4
Nic Pottier <nicpottier@gmail.com>
2012-03-31 09:10:07 -0700

    better base.html with twitter bootstrap 2, add git ignore, fix settings_common

--- nsms/skeleton/project/settings_common.py
+++ nsms/skeleton/project/settings_common.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'foo.db',                      # Or path to database file if using sqlite3.
+        'NAME': '!!PROJECT_NAME!!.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -124,7 +124,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.locale.LocaleMiddleware'
 )
 
-ROOT_URLCONF = 'foo.urls'
+ROOT_URLCONF = '!!PROJECT_NAME!!.urls'
 
 CACHES = {
     'default': {
