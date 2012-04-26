1da8185dfc453d93013146f4d6145d35bb8e88b4
Nic Pottier <nicpottier@gmail.com>
2012-04-26 11:08:51 +0200

    add nsms.lang app which takes care of activating and setting profile languages

--- nsms/utils/__init__.py
+++ nsms/utils/__init__.py
@@ -1,4 +1,37 @@
 from django.contrib.auth.models import User, Group
+from django.conf import settings
+import sys
+
+def import_from_string(kls):
+    """
+    Used to load a class object dynamically by name
+    """
+    parts = kls.split('.')
+    module = ".".join(parts[:-1])
+    m = __import__(module)
+    for comp in parts[1:]:
+        m = getattr(m, comp)            
+    return m
+
+def get_sms_profile(connection):
+    """
+    Given a connection, looks up the SMS profile using the SMS_PROFILE model registered in settings.py
+    """
+    profile_class_name = getattr(settings, 'SMS_PROFILE', None)
+
+    # no SMS_PROFILE set, then return nothing
+    if not profile_class_name:
+        return None
+
+    profile_class = import_from_string(profile_class_name)
+
+    # see if we can find a match for that connection
+    matches = profile_class.objects.filter(connection=connection)
+    if matches:
+        return matches[0]
+
+    else:
+        return None
 
 def get_connection_user(connection):
     """
