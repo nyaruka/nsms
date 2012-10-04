961a261eade861c444ba006a6eff95d36c063ae7
Nic Pottier <nicpottier@gmail.com>
2012-10-04 10:52:21 +0200

    add purty graph on message list page

--- nsms/console/views.py
+++ nsms/console/views.py
@@ -2,6 +2,8 @@ from smartmin.views import *
 from rapidsms_httprouter.models import Message
 from rapidsms_httprouter.router import get_router
 from django.utils.safestring import mark_safe
+from django.db.models import Count
+import datetime
 
 class MessageTesterForm(forms.Form):
     sender = forms.CharField(max_length=20, initial="12065551212")
@@ -49,10 +51,28 @@ class MessageCRUDL(SmartCRUDL):
             context = self.get_context_data(object_list=self.object_list)
 
             return self.render_to_response(context)
+
+        def build_daily_counts(self, objects, **filters):
+            counts = objects.filter(**filters).order_by('date').extra({'created':"date(date)"}).values('created').annotate(created_on_count=Count('id'))
+            
+            for count in counts:
+                created_str = count['created']
+                if not hasattr(created_str, 'year'):
+                    count['created'] = datetime.datetime.strptime(created_str, "%Y-%m-%d")
+
+            return counts
         
         def get_context_data(self, **kwargs):
             context = super(MessageCRUDL.List, self).get_context_data(**kwargs)
             
+            # get our queryset
+            objects = self.derive_queryset()
+            one_month = datetime.datetime.now() - datetime.timedelta(days=30)
+
+            # break it up by date counts
+            context['incoming_counts'] = self.build_daily_counts(objects, direction='I', date__gte=one_month)
+            context['outgoing_counts'] = self.build_daily_counts(objects, direction='O', date__gte=one_month)
+
             if self.request.method == 'POST':
                 context['form'] = MessageTesterForm(self.request.POST)
             else:
